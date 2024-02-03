4720d39703bb1894c234a95f0fc290603c5c5651
Dmitri49 <dmitrilissin@gmail.com>
2024-02-03 14:05:08 +0300

    add eslint

--- scripts/app.js
+++ scripts/app.js
@@ -1,35 +1,34 @@
-const modal = document.querySelector('#modal')
-const content = document.querySelector('#content')
-const backdrop = document.querySelector('#backdrop')
-
+const modal = document.querySelector('#modal');
+const content = document.querySelector('#content');
+const backdrop = document.querySelector('#backdrop');
 
 const technologies = [
-    {title: 'HTML', description: "HTML Text", type: 'html', done: false},
-    {title: 'CSS', description: "CSS Text", type: 'css', done: false},
-    {title: 'JavaScript', description: "JavaScript Text", type: 'javascript', done: false},
-    {title: 'Git', description: "Git Text", type: 'git', done: false},
-    {title: 'React', description: "React Text", type: 'react', done: false},
-]
+	{title: 'HTML', description: 'HTML Text', type: 'html', done: false},
+	{title: 'CSS', description: 'CSS Text', type: 'css', done: false},
+	{title: 'JavaScript', description: 'JavaScript Text', type: 'javascript', done: false},
+	{title: 'Git', description: 'Git Text', type: 'git', done: false},
+	{title: 'React', description: 'React Text', type: 'react', done: false},
+];
 
-content.addEventListener('click', openModal)
-backdrop.addEventListener('click', closeModal)
+content.addEventListener('click', openModal);
+backdrop.addEventListener('click', closeModal);
 
 function openModal() {
-    modal.classList.add('open')
+	modal.classList.add('open');
 }
 
 function closeModal() {
-    modal.classList.remove('open')
+	modal.classList.remove('open');
 }
 
-let html = ''
+let html = '';
 
 for (let i = 0; i < technologies.length; i++) {
-    html += `
+	html += `
         <div class="card">
             <h3>HTML</h3>
         </div>
-    `
+    `;
 }
 
-content.innerHTML += html
+content.innerHTML += html;
